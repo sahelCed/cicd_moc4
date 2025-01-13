c7454613d19c30fc0751f3c3da01bceab34c9634
Cedric Ait Sahel <ced.sahel@gmail.com>
2025-01-13 17:57:15 +0100

    test

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page2'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page3'),
     );
   }
 }
